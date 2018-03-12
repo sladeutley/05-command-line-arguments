8d6e78ac6465fd826dd0e3bbe674d550776f7ad0
Slade Utley <slade.utley@gmail.com>
2018-03-12 14:15:03 -0500

    able to get map to work

--- args.js
+++ args.js
@@ -10,8 +10,19 @@ console.log('num2',num2);
 const sum = +num1 + +num2;
 console.log('sum',sum);
 
-// const [ ,, ...args ] = process.argv;
+const [ ,, ...numbers ] = process.argv;
+console.log('numbers',numbers);
+
+let sumOfNumbers;
+// sumOfNumbers = numbers.map(number => +number.reduce((number1,number2) => (number1 + number2)));
+sumOfNumbers = numbers.map(number => +number);
+
+console.log('sumOfNumbers',sumOfNumbers);
+// numbers.map((number) => +(number));
+// console.log('numbers',numbers);
 // console.log('args',args);
-// const sumUp = args.reduce(a,b => {
-//     console.log('sumUp', a + b);
+// const sumUp = numbers.reduce((+a,+b) => {
+//     console.log('sumUp', +a + +b;
 // });
+// let sumUp = numbers;
+// console.log('sumUP',sumUP);
